a5820895b087e2c9960e168741856bf19365665d
최고운 <66200628+cku7808@users.noreply.github.com>
2022-10-19 21:13:42 +0900

    [Silver III] Title: N과 M (3), Time: 2136 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.py
@@ -0,0 +1,24 @@
+import sys
+n,m = map(int,sys.stdin.readline().split())
+arr = [i for i in range(1,n+1)] #1,2,3,4
+check = [False]*n
+
+result = []
+def dfs(cnt):
+    if cnt == m:
+        print(*result)
+        return
+    
+    for i in range(n):
+        if check[i]:
+            continue
+        else:
+            result.append(arr[i])
+            #check[i] = True
+            cnt += 1
+            dfs(cnt)
+            result.pop()
+            #check[i] = False
+            cnt -= 1
+
+dfs(0)
