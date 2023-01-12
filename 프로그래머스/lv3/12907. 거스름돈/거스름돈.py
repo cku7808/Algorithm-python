9829ac9b38eac33e92aca5ea27fceaa05120bb37
최고운 <66200628+cku7808@users.noreply.github.com>
2023-01-13 01:08:36 +0900

    [level 3] Title: 거스름돈, Time: 323.11 ms, Memory: 11.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/12907. 거스름돈/거스름돈.py
@@ -0,0 +1,11 @@
+from itertools import combinations_with_replacement
+
+def solution(n, money):
+    dp = [0]*(n+1)
+    dp[0] = 1
+    
+    for m in money:
+        for i in range(m, n+1):
+            dp[i] += dp[i-m]
+        
+    return dp[n]
