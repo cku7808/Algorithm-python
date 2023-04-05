7d1079043cea964e34a868ccc86ea96eaf740e2a
최고운 <66200628+cku7808@users.noreply.github.com>
2023-04-05 15:29:39 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 2.59 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/lv2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.py
+++ 프로그래머스/lv2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.py
@@ -1,24 +1,56 @@
 import re
 
-def solution(files):
-    mod_files = []
-    comp_char = re.compile('[a-z\-\. ]',re.I)
-    comp_num = re.compile('[0-9]')
+# def solution(files):
+#     mod_files = []
+#     comp_char = re.compile('[a-z\-\. ]',re.I)
+#     comp_num = re.compile('[0-9]')
     
-    for i in range(len(files)):
-        head, number, tail = "","",""
+#     for i in range(len(files)):
+#         head, number, tail = "","",""
         
-        for j in range(len(files[i])):
-            if comp_char.match(files[i][j]) and number == "":
-                head += files[i][j]
-            elif comp_num.match(files[i][j]):
-                number += files[i][j]
-            elif number != "" and comp_char.match(files[i][j]):
-                tail = files[i][len(head)+len(number):]
-                break
+#         for j in range(len(files[i])):
+#             if comp_char.match(files[i][j]) and number == "":
+#                 head += files[i][j]
+#             elif comp_num.match(files[i][j]):
+#                 number += files[i][j]
+#             elif number != "" and comp_char.match(files[i][j]):
+#                 tail = files[i][len(head)+len(number):]
+#                 break
 
-        mod_files.append((head,number,tail))
-    mod_files = sorted(mod_files, key = lambda x:(x[0].lower(),int(x[1])))
+#         mod_files.append((head,number,tail))
+#     mod_files = sorted(mod_files, key = lambda x:(x[0].lower(),int(x[1])))
     
-    answer = [h+n+t for h,n,t in mod_files]
+#     answer = [h+n+t for h,n,t in mod_files]
+#     return answer
+
+
+def solution(files):
+    result = []
+    for file in files:
+        first_num = re.findall(r"\d+", file)[0]
+        first_num_pos = file.index(first_num)
+        head = file[:first_num_pos]
+        number = file[len(head):len(head)+len(first_num)]
+        tail = file[len(number)+len(head):]
+        
+        result.append((head,number,tail))
+    result = sorted(result, key=lambda x:(x[0].lower(),int(x[1])))
+    answer = [h+n+t for h,n,t in result]
     return answer
+
+
+# 강사님 코드
+# def solution(files):
+#     answer = []
+#     my_files = []
+#     for idx, s in enumerate(files):
+#         number = re.findall(r"\d+",s)
+#         real_number = int(number[0])
+#         head = s[:s.index(number[0])]
+#         head = head.lower()
+#         my_files.append([head,real_number,idx])
+#     my_files.sort(key=lambda x:(x[0],x[1],x[2]))
+#     answer = [ files[ j[2] ] for j in my_files ]
+    
+#     return answer
+        
