8958a10115f8e00aec4ae218a9e290e547aa73ab
곤 <66200628+cku7808@users.noreply.github.com>
2023-10-10 19:05:43 +0900

    [Silver V] Title: 올림픽, Time: 44 ms, Memory: 31120 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.py
+++ 백준/Silver/8979. 올림픽/올림픽.py
@@ -7,16 +7,15 @@ for _ in range(n):
     tmp = list(map(int, sys.stdin.readline().split()))
     nation = tmp[0]
     score_list = tmp[1:]
-    score_list[0] *= 3
-    score_list[1] *= 2
-    score = sum(score_list)
 
-    lst[nation]=score
+    lst[nation]=score_list
 
-lst = dict(sorted(lst.items(),key=lambda x:x[1], reverse=True))
+lst = dict(sorted(lst.items(),key=lambda x:(x[1][0],x[1][1],x[1][2]), reverse=True))
+
 rank = 1
-for elem in lst.values():
-    if elem > lst[k]:
+for n,l in lst.items():
+    if n != k and l != lst[k]:
         rank += 1
+    else:
+        break
 print(rank)
-        
