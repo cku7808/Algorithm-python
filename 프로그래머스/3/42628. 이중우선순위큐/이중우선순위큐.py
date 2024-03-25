fb5489be1ed42e2b9b914d9216163d46790ee506
최고운 <cku7808@naver.com>
2024-03-25 18:56:32 +0900

    [level 3] Title: 이중우선순위큐, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
@@ -1,26 +1,17 @@
 import heapq
 def solution(operations):
-    arr = []
-    for command in operations:
-        if command[0] == "I":
-            heapq.heappush(arr, int(command[2:]))
-            # print("push %d result : "%(int(command[2:])),arr)
-        else:
-            if arr:
-                if command[2:] == "1":
-                    arr = list(map(lambda x:-x, arr))
-                    heapq.heapify(arr)
-                    num = heapq.heappop(arr)
-                    arr = list(map(lambda x:-x, arr))
-                    heapq.heapify(arr)
-                    # print("pop max %d result : "%(-num),arr)
-                else:
-                    num = heapq.heappop(arr)
-                    # print("pop min %d result : "%(num),arr)
-    if arr:
-        return [max(arr), min(arr)]
-    else:
-        return [0,0]
+    q = []
 
-                
-            
+    for op in operations:
+        if op == "D 1":
+            if q: q.pop()
+        elif op == "D -1":
+            if q: heapq.heappop(q)
+        else:
+            num = int(op.split(" ")[-1])
+            heapq.heappush(q, num)
+            q.sort()
+    if q:
+        return [q[-1], q[0]]
+    else:
+        return [0, 0]
