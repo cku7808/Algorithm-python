3f7669b7e46a65a542fd1528986f3ad3c8072a5d
최고운 <cku7808@naver.com>
2024-04-09 15:59:57 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.py
@@ -0,0 +1,8 @@
+import sys
+
+n, m = map(int, sys.stdin.readline().split())
+arr = list(range(n+1))
+for _ in range(m):
+    i, j = map(int, sys.stdin.readline().split())
+    arr[i:j+1] = arr[i:j+1][::-1]
+print(*arr[1:])
