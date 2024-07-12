37777d6cc1da1dc14d347198d426ce3e9dcf17b5
최고운 <cku7808@naver.com>
2024-07-12 15:26:20 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 석유 시추, Time: 51.93 ms, Memory: 13.8 MB -BaekjoonHub

--- 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/README.md
+++ 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/README.md
@@ -1,10 +1,10 @@
 # [level 2] [PCCP 기출문제] 2번 / 석유 시추 - 250136 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/250136#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/250136) 
 
 ### 성능 요약
 
-메모리: 14.7 MB, 시간: 67.45 ms
+메모리: 13.8 MB, 시간: 51.93 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 12일 14:27:23
+2024년 07월 12일 15:26:20
 
 ### 문제 설명
 
--- 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.py
+++ 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.py
@@ -8,42 +8,26 @@ def solution(land):
     dxs = [1,-1,0,0]
     dys = [0,0,1,-1]
     
-    # bfs로 석유 위치와 석유 덩어리 크기 구하기
-    pos_list = []
-    cnt = 2
+    visited = [[False]*col_num for _ in range(row_num)]
+    cols_score = [0]*col_num
+    
     for i in range(row_num):
         for j in range(col_num):
-            tmp = []
-            if land[i][j] == 1:
+            if land[i][j] == 1 and not visited[i][j]:
                 q = deque([(i,j)])
-                tmp.append((i,j))
-                land[i][j] = cnt
-                
+                visited[i][j] = True
+                cnt = 0
+                cols = set()
                 while q:
                     x, y = q.popleft()
+                    cols.add(y)
+                    cnt += 1
+
                     for dx, dy in zip(dxs, dys):
                         nx, ny = dx+x, dy+y
-                        if -1<nx<row_num and -1<ny<col_num and land[nx][ny] == 1:
+                        if -1<nx<row_num and -1<ny<col_num and land[nx][ny] == 1 and not visited[nx][ny]:
                             q.append((nx, ny))
-                            tmp.append((nx, ny))
-                            land[nx][ny] = cnt
-
-            if len(tmp):
-                pos_list.append(tmp)
-                cnt += 1
-
-    # 석유 덩어리 크기를 저장하기
-    size = dict()
-    for i in range(2, cnt):
-        size[i] = len(pos_list[i-2])
-
-    # 열 별로 순회하며 얻을 수 있는 석유 덩어리들의 크기를 더하기 
-    # transpose and summation
-    max_amount = 0
-    for elem in zip(*land):
-        tmp = 0
-        for num in set(elem):
-            if num != 0:
-                tmp += size[num]
-        max_amount = max(tmp, max_amount)
-    return max_amount
+                            visited[nx][ny] = True
+                for idx in cols:
+                    cols_score[idx] += cnt
+    return max(cols_score)                
