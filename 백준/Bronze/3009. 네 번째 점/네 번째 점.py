8e2a00a5444fe9fad3681b71239d54aef18116eb
최고운 <cku7808@naver.com>
2024-07-15 17:15:59 +0900

    [Bronze III] Title: 네 번째 점, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/3009. 네 번째 점/네 번째 점.py
@@ -0,0 +1,17 @@
+import sys
+
+x = []
+y = []
+for _ in range(3):
+    a, b = map(int, sys.stdin.readline().split())
+    if a not in x:
+        x.append(a)
+    else:
+        x.remove(a)
+
+    if b not in y:
+        y.append(b)
+    else:
+        y.remove(b)
+
+print(x[0], y[0])
