badd97f48cb9c493e4f8e42d1b99c8d44596a979
최고운 <cku7808@naver.com>
2024-07-23 14:54:50 +0900

    [Unrated] Title: [모의 SW 역량테스트] 활주로 건설, Time: 243 ms, Memory: 50,388 KB -BaekjoonHub

--- SWEA/Unrated/4014. ［모의 SW 역량테스트］ 활주로 건설/［모의 SW 역량테스트］ 활주로 건설.py
+++ SWEA/Unrated/4014. ［모의 SW 역량테스트］ 활주로 건설/［모의 SW 역량테스트］ 활주로 건설.py
@@ -6,25 +6,20 @@ def check(arr, x):
     for i in range(n-1):
         if arr[i] != arr[i+1]:
             if abs(arr[i]-arr[i+1]) != 1:
-                #print(i, i+1)
                 flag = False
             elif arr[i+1] > arr[i] :
-                #print(i, i+1, arr[max(0, i-x+1):i+1])
                 target = arr[max(0, i-x+1):i+1]
                 if len(target) < x or len(set(target)) != 1 or True in visited[max(0, i-x+1):i+1]:
                     flag = False
                 else:
                     visited[max(0, i-x+1):i+1] = [True]*len(target)
-                    #print(visited)
                     
             elif arr[i] > arr[i+1]:
                 target = arr[i+1:min(n, i+1+x)]
-                #print(i, i+1, arr[i+1:min(n, i+1+x)])
                 if len(target) < x or len(set(target)) != 1 or True in visited[i+1:min(n, i+1+x)]:
                     flag = False
                 else:
                     visited[i+1:min(n, i+1+x)] = [True]*len(target)
-                    #print(visited)
     return flag
 
 for i in range(1, T+1):
@@ -34,15 +29,11 @@ for i in range(1, T+1):
     
     for j in range(n):
         result1 = check(grid[j], x)
-        #print(result1)
         if result1:
-            #print(grid[j])
             cnt += 1
     
     for elem in zip(*grid):
         result2 = check(elem, x)
-        #print(result2)
         if result2:
-            #print(elem)
             cnt += 1
     print(f"#{i} {cnt}")
