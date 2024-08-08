366bf592a565df120b830e6cac319fc6fc8eb41f
최고운 <cku7808@naver.com>
2024-08-08 14:28:04 +0900

    [Silver III] Title: N과 M (7), Time: 3480 ms, Memory: 165644 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.py
@@ -0,0 +1,24 @@
+import sys
+
+n, m = map(int, sys.stdin.readline().split())
+arr = list(map(int, sys.stdin.readline().split()))
+
+final_arr = []
+tmp_arr = []
+
+def backtrack(tmp_arr, cnt):
+    global final_arr
+    if cnt == m:
+        final_arr.append(tuple(tmp_arr))
+        return
+    for i in range(n):
+        tmp_arr.append(arr[i])
+        backtrack(tmp_arr, cnt+1)
+        tmp_arr.pop()
+
+backtrack(tmp_arr, 0)
+final_arr = list(set(final_arr))
+final_arr.sort()
+
+for elem in final_arr:
+    print(*elem)
