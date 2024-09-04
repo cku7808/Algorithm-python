5710248c9462af209ff867b2564b87bb45bfcf4b
최고운 <cku7808@naver.com>
2024-09-04 12:24:18 +0900

    [Bronze I] Title: 색종이, Time: 1124 ms, Memory: 38304 KB, Score: 53 point -BaekjoonHub

--- 백준/Bronze/10163. 색종이/색종이.py
+++ 백준/Bronze/10163. 색종이/색종이.py
@@ -1,10 +1,14 @@
 graph = [[0]*1001 for _ in range(1001)]
 N = int(input())
+idx = 1
 for _ in range(N):
+
     start_x, start_y, width, height = map(int, input().split())
     for i in range(start_x, start_x+width):
         for j in range(start_y, start_y+height):
-            graph[i][j] += 1
+            graph[i][j] = idx
+    idx += 1
+cnt_lst = dict()
 
 for n in range(1, 1 + N):
     cnt = 0
@@ -13,4 +17,7 @@ for n in range(1, 1 + N):
 
             if graph[idx][jdx] == n:
                 cnt += 1
-    print(cnt)
+    cnt_lst[n] = cnt
+
+for ans in cnt_lst.keys():
+    print(cnt_lst[ans])
