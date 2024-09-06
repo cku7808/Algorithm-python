e70b54b95aa652d41d7f60aaa32ae26d285a39dc
최고운 <cku7808@naver.com>
2024-09-06 09:19:47 +0900

    [Gold IV] Title: 플로이드, Time: 592 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.py
+++ 백준/Gold/11404. 플로이드/플로이드.py
@@ -2,27 +2,27 @@ import sys
 
 n = int(sys.stdin.readline())
 m = int(sys.stdin.readline())
-
 inf = float("inf")
 graph = [[inf]*(n+1) for _ in range(n+1)]
 
 for _ in range(m):
-    v, u, w = map(int, sys.stdin.readline().split())
-    graph[v][u] = min(w, graph[v][u])
+    a, b, c = map(int, sys.stdin.readline().split())
+    graph[a][b] = min(c, graph[a][b])
 
-for i in range(1, v+1):
-    graph[i][i] = 0
+for i in range(1, n+1):
+    for j in range(1, n+1):
+        if i == j:
+            graph[i][j] = 0
 
 for k in range(1, n+1):
     for i in range(1, n+1):
         for j in range(1, n+1):
-            if i != j:
-                graph[i][j] = min(graph[i][j], graph[i][k]+graph[k][j])
+            graph[i][j] = min(graph[i][j], graph[i][k]+graph[k][j])
 
 for i in range(1, n+1):
     for j in range(1, n+1):
         if graph[i][j] == inf:
             graph[i][j] = 0
 
-for arr in graph[1:]:
-    print(*arr[1:])
+for elem in graph[1:]:
+    print(*elem[1:])
