0df50c912c658566db533b3727b07079863b49f1
최고운 <cku7808@naver.com>
2024-09-09 14:55:01 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 364 ms, Memory: 51444 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
@@ -1,30 +1,32 @@
-import sys
-import heapq
+import sys, heapq
+sys.setrecursionlimit(10**9)
 
-v,e = map(int, sys.stdin.readline().split())
+def find(x):
+    if parent[x] != x:
+        parent[x] = find(parent[x])
+    return parent[x]
 
+def union(a, b):
+    a, b = find(a), find(b)
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+
+v, e = map(int, sys.stdin.readline().split())
 graph = [[] for _ in range(v+1)]
-visited = [False]*(v+1)
-
-for _ in range(e):
-    a,b,c = map(int, sys.stdin.readline().split())
-    graph[a].append((c,b))
-    graph[b].append((c,a))
-
-
+parent = [i for i in range(v+1)]
+heap = []
 ans = 0
-heap = [(0,1)]
+for _ in range(e):
+    a, b, c = map(int, sys.stdin.readline().split())
+    heapq.heappush(heap, (c, a, b))
 
 while heap:
-    weight, node = heapq.heappop(heap)
-    if visited[node]:
-        continue
-    visited[node] = True
-    ans += weight
-
-    for w, u in graph[node]:
-        heapq.heappush(heap, (w,u))
+    cost, u, v = heapq.heappop(heap)
+    if find(u) != find(v):
+        # print(cost, u, v)
+        union(u, v)
+        ans += cost
 print(ans)
-    
-    
-    
