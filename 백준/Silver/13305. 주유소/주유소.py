533588c852d77bca7ffd72e4e2d3254eb1e6e79f
최고운 <cku7808@naver.com>
2024-09-09 16:09:38 +0900

    [Silver III] Title: 주유소, Time: 128 ms, Memory: 41844 KB, Score: 17 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.py
+++ 백준/Silver/13305. 주유소/주유소.py
@@ -1,14 +1,11 @@
-import sys
-
-n = int(sys.stdin.readline())
-length = list(map(int, sys.stdin.readline().split()))
-price = list(map(int, sys.stdin.readline().split()))
-
-ans = 0
-min_cost = price[0]
-for i in range(n-1):
-    if price[i]< min_cost:
-        min_cost = price[i]
-    ans += min_cost*length[i]
-
-print(ans)
+N = int(input())
+dist = list(map(int, input().split()))
+price = list(map(int, input().split()))
+dp = [0] * N
+dp[0] = dist[0] * price[0]
+max_dp = 0
+for i in range(1, N-1):
+    dp[i] = min(dp[i-1] + (price[i-1] * dist[i]), dp[i-1] + (price[i] * dist[i]))
+    if dp[i] > max_dp:
+        max_dp = dp[i]
+print(max_dp)
