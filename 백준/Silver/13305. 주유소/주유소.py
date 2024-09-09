cbdb6a4467039b702dd6c2019b177af3bd734b3e
최고운 <cku7808@naver.com>
2024-09-09 16:50:53 +0900

    [Silver III] Title: 주유소, Time: 96 ms, Memory: 46788 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.py
+++ 백준/Silver/13305. 주유소/주유소.py
@@ -1,11 +1,19 @@
 N = int(input())
 dist = list(map(int, input().split()))
 price = list(map(int, input().split()))
-dp = [0] * N
-dp[0] = dist[0] * price[0]
-max_dp = 0
-for i in range(1, N-1):
-    dp[i] = min(dp[i-1] + (price[i-1] * dist[i]), dp[i-1] + (price[i] * dist[i]))
-    if dp[i] > max_dp:
-        max_dp = dp[i]
-print(max_dp)
+
+cost = 0
+tmp = price[0]
+tmp_len = dist[0]
+
+for idx in range(1, N-1):
+    if tmp < price[idx]:
+         tmp_len += dist[idx]
+    else:
+        cost += tmp * tmp_len
+        tmp = price[idx]
+        tmp_len = dist[idx]
+else:
+    cost += tmp*tmp_len
+
+print(cost)
