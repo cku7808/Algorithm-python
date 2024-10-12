7d881688d1bb9d7c35e0fa0fc81a7dae4f7bed8b
최고운 <cku7808@naver.com>
2024-10-12 23:56:38 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 320 ms, Memory: 50356 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
@@ -1,36 +1,31 @@
 import sys, heapq
+input = sys.stdin.readline
 
 def find(x):
-    # if parent[x] != x:
-    #     parent[x] = find(parent[x])
-    # return parent[x]
-    while x != parent[x]: # 3 4
+    while x != parent[x]:
         parent[x] = parent[parent[x]]
         x = parent[x]
     return x
 
-
 def union(a, b):
-    a, b = find(a), find(b)
-    if a < b:
-        parent[b] = a
-    else:
+    a = find(a)
+    b = find(b)
+    if a > b:
         parent[a] = b
+    else:
+        parent[b] = a
 
-
-v, e = map(int, sys.stdin.readline().split())
-graph = [[] for _ in range(v+1)]
-parent = [i for i in range(v+1)]
+v, e = map(int, input().split())
+parent = list(range(v+1))
 heap = []
-ans = 0
 for _ in range(e):
-    a, b, c = map(int, sys.stdin.readline().split())
+    a, b, c = map(int, input().split())
     heapq.heappush(heap, (c, a, b))
 
+total_weight = 0
 while heap:
-    cost, u, v = heapq.heappop(heap)
-    if find(u) != find(v):
-        # print(cost, u, v)
-        union(u, v)
-        ans += cost
-print(ans)
+    cost, x, y = heapq.heappop(heap)
+    if find(x) != find(y):
+        union(x, y)
+        total_weight += cost
+print(total_weight)
